cf572ac87520979a6dd1aa1f242eddefe735f704
Neno-N <thatchickonsimstea@gmail.com>
2022-06-21 13:38:01 +0200

    completed commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,9 @@
 const menuBtn = document.querySelector('.nav-menu__btn')
 const menuExitBtn = document.querySelector('.nav-exit-menu-btn')
 const navMenu = document.querySelector('.nav')
-
-const videos = document.querySelectorAll('.card__vid');
+const videos = document.querySelectorAll('.slider-card__vid');
+const scrollContainer = document.querySelector('.video-slider');
+const transitionEl = document.querySelector('.transition');
 
 //Active page
 const activePage  = window.location.pathname;
@@ -12,6 +13,17 @@ const navLink = document.querySelectorAll('.nav-link')
 
 
 /*======= End of Variables ======*/
+
+/*======= Onload =======*/
+window.onload = () =>{
+    setTimeout(() => {
+            transitionEl.classList.remove('is-active')
+            transitionEl.classList.remove('is-active2')
+
+    }, 1000);
+}
+/*======= End of Onload =======*/
+
 
 /*======= Functions =======*/
 const toggleMenu = () => {
@@ -40,9 +52,24 @@ navLink.forEach(link=>{
 //Play/Pause Vid when hover
 videos.forEach(vid=>{
     vid.addEventListener('mouseenter',()=>{
+        //dim other videos
+        videos.forEach(item=>{
+            item.classList.add('vid-dim')
+        })
+
+        vid.classList.remove('vid-dim')
         vid.play()
+
+        
     })
     vid.addEventListener('mouseout',()=>{
+        //dim other videos
+        videos.forEach(item=>{
+            if(item.classList.contains('vid-dim')){
+                item.classList.remove('vid-dim')
+            }
+            
+        })
 
         //Video starts again
         vid.pause();
@@ -50,4 +77,17 @@ videos.forEach(vid=>{
     })
 })
 
+//horizontal scrolling
+window.addEventListener("wheel", function (e) {
+    if(e.deltaY > 0){
+        scrollContainer.scrollLeft += 100;
+    } else{
+        scrollContainer.scrollLeft -= 100;
+    }
+  });
+
 /*======= End of Event Listeners =======*/
+
+
+
+  
